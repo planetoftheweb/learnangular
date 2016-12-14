130dbffa125ab4b074f01638d167cfa8c223bfc2
Ray Villalobos <ray@planetoftheweb.com>
2016-12-13 17:33:55 -0800

    deleted app files

--- app/app.boot.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.modules';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
- 
--- app/component.app.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app',
-  template: '<h1>Artist Directory</h1>'
-})
-
-export class AppComponent {}
