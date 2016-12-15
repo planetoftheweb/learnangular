25ce7197b1fc52999efdf4ebcdac5ec6026157d2
Ray Villalobos <ray@planetoftheweb.com>
2016-12-14 16:10:27 -0800

    02_02e Displaying Data

--- app/component.app.ts
+++ app/component.app.ts
@@ -5,4 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './partials/app.html' 
 })
 
-export class AppComponent {}
+export class AppComponent {
+  name: string;
+  artists: any;
+
+  constructor() {
+    this.name = 'Xhou Ta';
+    this.artists = [
+      {
+        name: 'Barot Bellingham',
+        school: 'Penn State'
+      }, {
+        name: 'Jonathan Ferrar',
+        school: 'University of Illinois'
+      }, {
+        name: 'Hillary Post',
+        school: 'University of Florida'
+      } 
+    ]
+  }
+}
